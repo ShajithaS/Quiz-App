b90e9a5161bda14fe85f287c4c3b12467f7ceb29
Shajitha S <shajitha090204@gmail.com>
2025-05-29 19:14:26 +0530

    Initial commit

---
+++ quiz-app/src/assets/data.js
@@ -0,0 +1,43 @@
+export const data = [
+    {
+      question: "What does HTML stand for?",
+      option1: "Hyperlinks and Text Markup Language",
+      option2: "Home Tool Markup Language",
+      option3: "Hyper Text Markup Language",
+      option4: "Hyper Tool Multi Language",
+      ans: 3
+    },
+    {
+      question: "Which tag is used to create a hyperlink in HTML?",
+      option1: "<link>",
+      option2: "<a>",
+      option3: "<href>",
+      option4: "<hyper>",
+      ans: 2
+    },
+    {
+      question: "Which language is used to style a web page?",
+      option1: "HTML",
+      option2: "jQuery",
+      option3: "CSS",
+      option4: "Python",
+      ans: 3
+    },
+    {
+      question: "What does DOM stand for in web development?",
+      option1: "Document Object Model",
+      option2: "Data Object Management",
+      option3: "Digital Order Model",
+      option4: "Document Output Module",
+      ans: 1
+    },
+    {
+      question: "Which of the following is a JavaScript framework?",
+      option1: "Django",
+      option2: "Laravel",
+      option3: "React",
+      option4: "Flask",
+      ans: 3
+    }
+  ];
+  
